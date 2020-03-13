d04ddea95d304bc118c62c202e093f780e9e734a
yzgang <yzgang76@sina.com>
2020-03-13 11:34:16 +0800

    logback and kafkautil

--- src/main/scala/com/hpe/zg/util/KafkaUtil.scala
+++ src/main/scala/com/hpe/zg/util/KafkaUtil.scala
@@ -5,26 +5,35 @@ import java.util
 import java.util.Properties
 
 import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.kafka.clients.admin.{AdminClient, CreateTopicsResult, DeleteTopicsResult, KafkaAdminClient, NewTopic, TopicListing}
 import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
-import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.kafka.common.TopicPartition
 import org.slf4j.{Logger, LoggerFactory}
 
 object KafkaUtil {
     val logger: Logger = LoggerFactory.getLogger(KafkaUtil.getClass)
     logger.info(s"Init KafkaUtil")
-    private def createProperties(config: Config, group: String, isConsumer: Boolean): Properties = {
+
+    val bootstrapServer = "localhost:9092"
+
+
+    private def createCommonProperties: Properties = {
         val props = new Properties()
-        props.put("bootstrap.servers", "localhost:9092" /*config.getString("kafka.bootstrap-server") + ":" + config.getInt("kafka.port")*/)
+        props.put("bootstrap.servers", bootstrapServer)
         props.put("retries", 0.asInstanceOf[Object])
-        props.put("batch.size", 16384.asInstanceOf[Object])
-        props.put("buffer.memory", 33554432.asInstanceOf[Object])
-        //        props.put("linger.ms", 1)
+        props
+    }
+
+    private def createProperties(config: Config, group: String, isConsumer: Boolean): Properties = {
+        val props = createCommonProperties
+        props.put("batch.size", 16384)
+        props.put("buffer.memory", 33554432)
         if (isConsumer) {
-            props.put("group.id", "OSSM_BE_CLUSTER" + group)
+            props.put("group.id", group)
             props.put("enable.auto.commit", "true")
             props.put("auto.commit.interval.ms", "1000")
-            props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest") //for test
+            props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest") //for test
             props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
             props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
         } else {
@@ -33,6 +42,47 @@ object KafkaUtil {
         }
         props
     }
+
+    def getAdmin: AdminClient = {
+        val props = createCommonProperties
+        //        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+        //        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+        AdminClient.create(props) //need to be closed by caller
+    }
+
+    def createTopic(admin: AdminClient, topic: String, numPartitions: Int, replicationFactor: Short): CreateTopicsResult = {
+        logger.debug(s"create new topic $topic, $numPartitions, $replicationFactor")
+        admin.createTopics(util.Arrays.asList(new NewTopic(topic, numPartitions, replicationFactor)))
+    }
+
+    def createTopic(topic: String, numPartitions: Int, replicationFactor: Short): CreateTopicsResult = {
+        logger.debug(s"create new topic $topic, $numPartitions, $replicationFactor")
+        val create: AdminClient = getAdmin
+        val result = create.createTopics(util.Arrays.asList(new NewTopic(topic, numPartitions, replicationFactor)))
+        create.close()
+        result
+    }
+
+    /*  def deleteTopic(admin: AdminClient, topic: String): DeleteTopicsResult = {
+          admin.deleteTopics(util.Arrays.asList(topic))
+      }
+
+      def deleteTopic(topic: String): DeleteTopicsResult = {
+          val delete: AdminClient = getAdmin
+          val result = delete.deleteTopics(util.Arrays.asList(topic))
+          delete.close()
+          result
+      }*/
+
+    def getTopics(admin: AdminClient): util.Collection[TopicListing] = admin.listTopics().listings().get()
+
+
+    def listTopic(): Unit = {
+        val admin: AdminClient = getAdmin
+        admin.listTopics().listings().get().forEach(println)
+        admin.close()
+    }
+
 
     def createProcedure(conf: Config): KafkaProducer[String, java.io.Serializable] = new KafkaProducer[String, java.io.Serializable](createProperties(conf, null, isConsumer = false))
 
@@ -69,30 +119,58 @@ object KafkaUtil {
        }
     */
     def main(args: Array[String]): Unit = {
+        val admin = getAdmin
+        //        createTopic(admin, "p1", 2, 1)
+        getTopics(admin).forEach(println)
+        admin.close()
+
         val conf = ConfigFactory.load("management")
-        val consumer = createConsumer(conf, "g3")
+        val consumer = createConsumer(conf, "group13332")
         val producer = createProcedure(conf)
 
-        val topic = "OSSM_BE_METRICS"
-//        val key_record = conf.getString("ossm.monitor.keys.record")
+        val topic = "mytest"
+        val topic2 = "p1"
+        val key1 = "key1"
+        val key2 = "key2"
 
-        //        consumer.subscribe(util.Arrays.asList(topic))
+//                consumer.subscribe(util.Arrays.asList(topic))
 
-        //        for (c <- 1 to 10) producer.send(new ProducerRecord[String, java.io.Serializable](topic, key_record, c.toString))
+        //        for (c <- 11 to 15) producer.send(new ProducerRecord[String, java.io.Serializable](topic, key1, c.toString))
+        //        for (c <- 15 to 20) producer.send(new ProducerRecord[String, java.io.Serializable](topic, key2, c.toString))
+
+//        for (c <- 1 to 5) {
+//            val v = c * 2 + 30
+//            producer.send(new ProducerRecord[String, java.io.Serializable](topic, key1, v.toString))
+//            producer.send(new ProducerRecord[String, java.io.Serializable](topic, key2, (v+1).toString))
+//        }
         import scala.jdk.CollectionConverters._
 
         val m = consumer.partitionsFor(topic).asScala
+        println(s"********************  partitions")
         m.foreach(println)
+        val m1 = consumer.partitionsFor(topic2).asScala
+        println(s"********************  partitions")
+        m1.foreach(println)
+        //
 
-        val p = new TopicPartition(topic, 0)
-        consumer.assign(util.Arrays.asList(p))
-        consumer.seek(p, 0)
+        val p0 = new TopicPartition(topic, 0)
+
+        val p20 = new TopicPartition(topic2, 0)
+        val p21 = new TopicPartition(topic2, 1)
+
+        consumer.assign(util.Arrays.asList(p0, p20,p21))
+
+        consumer.seek(p0, 10)
+        consumer.seek(p20, 74)
+        consumer.seek(p21, 95)
 
 
+
+        println(s"data")
         while (true) {
             val records = consumer.poll(Duration.ofMillis(100)).asScala
             for (record <- records) {
-                println(s"${record.partition}-${record.offset()} : ${record.key}->${record.value}")
+                println(s"[${record.topic()}] ${record.partition}-${record.offset()} : ${record.key}->${record.value}")
             }
             Thread.sleep(1000)
         }
