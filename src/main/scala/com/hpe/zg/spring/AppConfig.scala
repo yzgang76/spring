17da6f9364efb8f4f6566c615cfe69b105a7a7ea
yzgang <yzgang76@sina.com>
2020-03-20 13:18:28 +0800

    integrate swagger

--- src/main/scala/com/hpe/zg/spring/AppConfig.scala
+++ src/main/scala/com/hpe/zg/spring/AppConfig.scala
@@ -7,18 +7,52 @@ import akka.actor.typed.scaladsl.AskPattern._
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpMethods, HttpRequest, HttpResponse}
 import akka.stream.Materializer
+
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.util.{Failure, Success}
 import com.alibaba.fastjson.{JSON, JSONObject}
 import com.typesafe.config.ConfigFactory
+import io.swagger.annotations.{ApiImplicitParams, ApiImplicitParam, ApiOperation}
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.{Bean, Configuration}
 import org.springframework.context.support.ClassPathXmlApplicationContext
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.{PathVariable, ResponseBody}
 import org.springframework.web.context.request.async.DeferredResult
+import springfox.documentation.builders.{ApiInfoBuilder, PathSelectors, RequestHandlerSelectors}
+import springfox.documentation.service.{ApiInfo, Contact}
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+
+@EnableSwagger2
+@Configuration
+class Swagger2Config {
+    @Bean
+    def createRestApi: Docket = {
+        new Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(apiInfo)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.hpe.zg.spring"))
+            .paths(PathSelectors.any())
+            .build()
+    }
+
+    private def apiInfo: ApiInfo = {
+
+        new ApiInfoBuilder()
+            .title("Swagger Test")
+            .description("REST-API-Doc")
+            .termsOfServiceUrl("http://localhost/")
+            .version("1.0.0") //接口文档的版本
+            .contact(new Contact("zhigang", "", "zhi-gang.yan@hpe.com"))
+            .build
+    }
+}
 
 @SpringBootApplication
 class AppConfig {
@@ -79,6 +113,7 @@ import org.springframework.web.bind.annotation.{RequestMapping, RestController}
 @RestController
 class indexController {
     @RequestMapping(value = Array("/index"))
+    @ApiOperation(value = "basic test", httpMethod = "GET", notes = "nnnn1", response = classOf[JSONObject])
     def index(): JSONObject = {
         val json = new JSONObject
         json.put("code", 0)
@@ -86,45 +121,23 @@ class indexController {
         json
     }
 
-    def httpGet(uri: String): DeferredResult[JSONObject] = {
-        import akka.actor.typed.scaladsl.adapter._
-        val result = new DeferredResult[JSONObject](6 * 1000L)
-        result.onTimeout(() => {
-            println(s"DeferredResult overtime")
-            val json = new JSONObject
-            json.put("code", 500)
-            json.put("error", "timeout")
-            result.setResult(json)
-        })
-        val responseFuture: Future[HttpResponse] =
-            Http()(MyApplication.system.toClassic).singleRequest(HttpRequest(uri = uri, method = HttpMethods.GET))
-
-        implicit val ec: ExecutionContextExecutor = MyApplication.system.executionContext
-        implicit val mat: Materializer = Materializer(MyApplication.system)
-
-        import akka.http.scaladsl.unmarshalling.Unmarshal
-        responseFuture.onComplete {
-            case Success(res) => Unmarshal(res.entity).to[String].onComplete {
-                case Success(str) =>
-                    result.setResult(JSON.parseObject(str))
-                case Failure(e) => result.setErrorResult(e.toString)
-            }
-
-            case Failure(e) => result.setErrorResult(e.toString)
-        }
-        result
-    }
+    import com.hpe.zg.util.Utils._
 
     @RequestMapping(value = Array("/info"))
     @ResponseBody
+    @ApiOperation(value = "basic get info", httpMethod = "GET", notes = "nnnn2", response = classOf[JSONObject])
     def info(): DeferredResult[JSONObject] = {
         httpGet("http://localhost:8082/info")
     }
 
     @RequestMapping(value = Array("/info/{host}"))
     @ResponseBody
+    @ApiOperation(value = "get info with host param", httpMethod = "GET", notes = "nnnn3", response = classOf[JSONObject])
+    @ApiImplicitParams(
+        value= Array(new ApiImplicitParam(name="host", value="host name", dataType = "path", required = true))
+    )
     def info(@PathVariable host: String): DeferredResult[JSONObject] = {
-        httpGet(s"http://${host}:8082/info")
+        httpGet(s"http://$host:8082/info")
     }
 }
 
@@ -132,6 +145,7 @@ class indexController {
 class indexController2 {
     @RequestMapping(value = Array("/akka"))
     @ResponseBody
+    @ApiOperation(value = "akka test", httpMethod = "GET", notes = "nnnn", response = classOf[JSONObject])
     def akka(): DeferredResult[JSONObject] = {
         import MyApplication.system
         import com.hpe.zg.spring.MyAkkaSystem._
---
+++ src/main/scala/com/hpe/zg/util/Utils.scala
@@ -0,0 +1,42 @@
+package com.hpe.zg.util
+
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpMethods, HttpRequest, HttpResponse}
+import akka.stream.Materializer
+import com.alibaba.fastjson.{JSON, JSONObject}
+import com.hpe.zg.spring.MyApplication
+import org.springframework.web.context.request.async.DeferredResult
+
+import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.util.{Failure, Success}
+
+object Utils {
+    def httpGet(uri: String): DeferredResult[JSONObject] = {
+        import akka.actor.typed.scaladsl.adapter._
+        val result = new DeferredResult[JSONObject](6 * 1000L)
+        result.onTimeout(() => {
+            println(s"DeferredResult overtime")
+            val json = new JSONObject
+            json.put("code", 500)
+            json.put("error", "timeout")
+            result.setResult(json)
+        })
+        val responseFuture: Future[HttpResponse] =
+            Http()(MyApplication.system.toClassic).singleRequest(HttpRequest(uri = uri, method = HttpMethods.GET))
+
+        implicit val ec: ExecutionContextExecutor = MyApplication.system.executionContext
+        implicit val mat: Materializer = Materializer(MyApplication.system)
+
+        import akka.http.scaladsl.unmarshalling.Unmarshal
+        responseFuture.onComplete {
+            case Success(res) => Unmarshal(res.entity).to[String].onComplete {
+                case Success(str) =>
+                    result.setResult(JSON.parseObject(str))
+                case Failure(e) => result.setErrorResult(e.toString)
+            }
+
+            case Failure(e) => result.setErrorResult(e.toString)
+        }
+        result
+    }
+}
