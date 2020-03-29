8983d706c424e5731ef1adaebb84675801edf7c3
yzgang <yzgang76@sina.com>
2020-03-29 13:57:01 +0800

    dim simulator

--- src/main/scala/com/hpe/zg/ossm/dimension/DimensionEngine.scala
+++ src/main/scala/com/hpe/zg/ossm/dimension/DimensionEngine.scala
@@ -1,11 +1,10 @@
 package com.hpe.zg.ossm.dimension
 
-import java.text.SimpleDateFormat
 import java.util
 
 import com.hpe.zg.util.Utils
 import org.json.JSONObject
-import org.slf4j.{Logger, LoggerFactory}
+import org.slf4j.LoggerFactory
 
 import scala.io.{Codec, Source}
 
@@ -27,34 +26,9 @@ object DimensionFactory {
     }
 }
 
-case class Field(name: String, `type`: String, unique: Boolean, enum: Option[util.ArrayList[AnyRef]], format: Option[String]) {
-    val logger: Logger = LoggerFactory.getLogger(classOf[Field])
-
-    def randomValue[T](list: util.ArrayList[T]): T = list.get((math.random() * list.size).round.toInt)
-
-
-    def createValue(): (String, AnyRef) = {
-        `type` match {
-            case "String" =>
-                if (unique) (name, name + System.currentTimeMillis)
-                else if (enum.isDefined) (name, randomValue(enum.get))
-                else (name, name)
-            case "long" => (name, long2Long(System.currentTimeMillis))
-            case "int" => (name, int2Integer((math.random * 100.0).round.toInt))
-            case "double" => (name, double2Double(math.random * 1000.0))
-            case "float" => (name, float2Float((math.random * 1000.0).toFloat))
-            case "date" =>
-                val d: SimpleDateFormat = try {
-                    new SimpleDateFormat(format.getOrElse("yyyy-MM-dd HH:mm:ss"))
-                } catch {
-                    case e: IllegalArgumentException =>
-                        logger.error(s"Wrong format ${format.get} in field $name. $e\n use default 'yyyy-MM-dd HH:mm:ss'")
-                        new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-                }
-                (name, d.format(System.currentTimeMillis))
-            case "boolean" => (name, boolean2Boolean(math.random > 0.5))
-        }
-    }
+case class DimensionData(name: String,
+                         ts: Long,
+                         data: util.HashMap[String, java.io.Serializable]) extends java.io.Serializable {
 }
 
 object Dimension {
@@ -62,14 +36,35 @@ object Dimension {
         new Dimension(obj.toMap)
     }
 
-    def simData(): util.Map[java.lang.String, AnyRef] = {
-        null
-    }
 }
 
 class Dimension(map: util.Map[java.lang.String, AnyRef]) {
+    private val logger = LoggerFactory.getLogger(classOf[Dimension])
 
     import scala.jdk.CollectionConverters._
+
+    val name: String = map.get("name").toString
+    val version: String = map.get("version").toString
+    val fields: List[Field] = {
+        map.get("fields") match {
+            case list: util.ArrayList[AnyRef] => list.asScala
+                .collect({
+                    case m: util.HashMap[String, java.io.Serializable] => Field.fromMap(m).orNull
+                    case o: Any =>
+                        logger.warn(s"Invalid definition of field $o")
+                        null
+                })
+                .filter(f => f != null)
+                .toList
+            case _ => List.empty
+        }
+    }
+
+    def generateValue(): DimensionData = {
+        val map = new util.HashMap[String, java.io.Serializable]()
+        fields.foreach(f => map.put(f.name, f.createValue()))
+        DimensionData(name, System.currentTimeMillis(), map)
+    }
 
     def formatPrint(): Unit = formatPrint(map, 0)
 
@@ -94,17 +89,19 @@ class Dimension(map: util.Map[java.lang.String, AnyRef]) {
             case _ =>
                 println(s"$ob")
         }
+
+    override def toString: String =
+        s"name= $name\nField:\n" + (for (field <- fields) yield field.toString).mkString("\n")
 }
 
 object Test extends App {
-    val dd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-    println(dd.format(System.currentTimeMillis))
-
     val path = Test.getClass.getResource("/").toString + "graph.json"
-    val path2 = """C:\mycode\spring\target\classes\graph.json"""
+    val path2 = """C:\mycode\spring\target\classes\temip_raw.json"""
     val d = DimensionFactory.createDimension(path2)
     d match {
-        case Some(v) => v.formatPrint()
+        case Some(v) =>
+            println(v)
+            println(v.generateValue())
         case None => println(s"Failed to create Dimension")
     }
 }
---
+++ src/main/scala/com/hpe/zg/ossm/dimension/Field.scala
@@ -0,0 +1,92 @@
+package com.hpe.zg.ossm.dimension
+
+import java.text.SimpleDateFormat
+
+import org.json.JSONObject
+import org.slf4j.LoggerFactory
+
+object Field {
+    private val logger = LoggerFactory.getLogger(classOf[Field])
+
+    def fromMap(map: java.util.HashMap[String, java.io.Serializable]): Option[Field] = {
+        try {
+            Some(Field(
+                name = map.get("name").toString,
+                `type` = map.get("type").toString,
+                unique = map.getOrDefault("unique", false).asInstanceOf[Boolean],
+                enum = try {
+                    val s = map.get("enum").toString
+                    Some(s.split(","))
+                } catch {
+                    case _: Throwable => None
+                },
+                format = try Some(map.get("format").toString) catch {
+                    case _: Throwable => None
+                })
+            )
+        } catch {
+            case e: Throwable =>
+                logger.error(s"Wrong in field definition $map, $e")
+                None
+        }
+    }
+
+   /* def fromJson(str: String): Option[Field] = {
+        try {
+            val ob = new JSONObject(str)
+            Some(Field(
+                name = ob.getString("name"),
+                `type` = ob.getString("type"),
+                unique = try ob.getBoolean("unique") catch {
+                    case _: Throwable => false
+                },
+                enum = try {
+                    val s = ob.getString("enum")
+                    Some(s.split(","))
+                } catch {
+                    case _: Throwable => None
+                },
+                format = try Some(ob.getString("format")) catch {
+                    case _: Throwable => None
+                })
+            )
+        } catch {
+            case e: Throwable =>
+                logger.error(s"Wrong in field definition $str, $e")
+                None
+        }
+    }*/
+}
+
+case class Field(name: String, `type`: String, unique: Boolean, enum: Option[Array[String]], format: Option[String]) {
+    private val logger = LoggerFactory.getLogger(classOf[Field])
+
+    private def randomValue[T](list: Array[T]): T = list((math.random() * list.length).round.toInt)
+
+    def createValue(): java.io.Serializable = {
+        `type` match {
+            case "string" =>
+                if (unique)  name + System.currentTimeMillis
+                else if (enum.isDefined) randomValue(enum.get)
+                else  name
+            case "long" =>  long2Long(System.currentTimeMillis)
+            case "int" => int2Integer((math.random * 100.0).round.toInt)
+            case "double" =>  double2Double(math.random * 1000.0)
+            case "float" =>float2Float((math.random * 1000.0).toFloat)
+            case "date" =>
+                val d: SimpleDateFormat = try {
+                    new SimpleDateFormat(format.getOrElse("yyyy-MM-dd HH:mm:ss"))
+                } catch {
+                    case e: IllegalArgumentException =>
+                        logger.error(s"Wrong format ${format.get} in field $name. $e\n use default 'yyyy-MM-dd HH:mm:ss'")
+                        new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                }
+                d.format(System.currentTimeMillis)
+            case "boolean" => boolean2Boolean(math.random > 0.5)
+            case _ =>throw new IllegalArgumentException(s"not support the type ${`type`}")
+        }
+    }
+
+    override def toString: String = s"name = $name, type = ${`type`}"
+}
+
